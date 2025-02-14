67c030934dbb2d1e427138f01619e8fd06bfde1c
hmbashar <hmbashar@gmail.com>
2025-02-14 19:15:38 +0600

    added more attr

--- includes/Shortcode.php
+++ includes/Shortcode.php
@@ -43,29 +43,77 @@ class Shortcode
             'posts_per_page' => -1,
             'orderby' => 'date',
             'order' => 'DESC',
-            'link_text' => 'Click to View Photos'
+            'link_text' => esc_html__('Click to View Photos', 'gallery-tools-for-annmoyna'),
+            'post_ids' => '',
+            'title_color' => '',
+            'title_font_size' => '',
+            'item_bg_color' => '',
+            'date_color' => '',
+            'date_font_size' => ''
         ), $atts);
 
-        $query = new \WP_Query(array(
+        $query_args = array(
             'post_type' => PostType::POST_TYPE,
             'posts_per_page' => $atts['posts_per_page'],
             'orderby' => $atts['orderby'],
             'order' => $atts['order']
-        ));
+        );
+
+        // Add post ID filtering if specified
+        if (!empty($atts['post_ids'])) {
+            $post_ids = array_map('trim', explode(',', $atts['post_ids']));
+            $query_args['post__in'] = $post_ids;
+        }
+
+        $query = new \WP_Query($query_args);
 
         ob_start();
 
         if ($query->have_posts()):
+            // Only output styles if custom values are provided
+            $custom_styles = array();
+            if (!empty($atts['item_bg_color'])) {
+                $custom_styles[] = '.gtfa-gallery-item { background: ' . esc_attr($atts['item_bg_color']) . '; }';
+            }
+            if (!empty($atts['title_color']) || !empty($atts['title_font_size'])) {
+                $title_style = '.gtfa-gallery-item-normal .gtfa-gallery-title {';
+                if (!empty($atts['title_color'])) {
+                    $title_style .= ' color: ' . esc_attr($atts['title_color']) . ';';
+                }
+                if (!empty($atts['title_font_size'])) {
+                    $title_style .= ' font-size: ' . esc_attr($atts['title_font_size']) . ';';
+                }
+                $title_style .= ' }';
+                $custom_styles[] = $title_style;
+            }
+            
+            if (!empty($atts['date_color']) || !empty($atts['date_font_size'])) {
+                $date_style = '.gtfa-gallery-item-normal .gtfa-gallery-date {';
+                if (!empty($atts['date_color'])) {
+                    $date_style .= ' color: ' . esc_attr($atts['date_color']) . ';';
+                }
+                if (!empty($atts['date_font_size'])) {
+                    $date_style .= ' font-size: ' . esc_attr($atts['date_font_size']) . ';';
+                }
+                $date_style .= ' }';
+                $custom_styles[] = $date_style;
+            }
+            
+            if (!empty($custom_styles)) :
             ?>
+            <style>
+                <?php echo implode("\n                ", $custom_styles); ?>
+            </style>
+            <?php endif; ?>
             <div class="gtfa-gallery-grid">
                 <?php
                 while ($query->have_posts()):
                     $query->the_post();
                     $date = get_post_meta(get_the_ID(), '_gtfa_gallery_date', true);
-                    //   $shortcode = get_post_meta(get_the_ID(), '_gtfa_gallery_shortcode', true);
                     $gallery_id = get_post_meta(get_the_ID(), '_gtfa_envira_gallery_id', true);
 
-                    // Format date with ordinal suffix
+                    // Format date with ordinal suffix only if date exists
+                    $formatted_date = '';
                     if ($date) {
                         $timestamp = strtotime($date);
                         $day = date('j', $timestamp);
@@ -91,8 +139,8 @@ class Shortcode
                                     </div>
                                 <?php endif;
                             endif; ?>
-                            <h3 class="gtfa-gallery-title"><?php the_title(); ?></h3>
-                            <?php if ($date): ?>
+                            <h3 class="gtfa-gallery-title"><?php echo esc_html(get_the_title()); ?></h3>
+                            <?php if (!empty($formatted_date)): ?>
                                 <div class="gtfa-gallery-date">
                                     <?php echo esc_html($formatted_date); ?>
                                 </div>
