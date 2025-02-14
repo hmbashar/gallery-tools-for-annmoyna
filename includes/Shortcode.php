b79067ac63a9d8b0b9c58099954b6c9bbc5884a9
hmbashar <hmbashar@gmail.com>
2025-02-14 17:36:34 +0600

    date formate changed

--- includes/Shortcode.php
+++ includes/Shortcode.php
@@ -78,12 +78,23 @@ class Shortcode {
                     $date = get_post_meta(get_the_ID(), '_gtfa_gallery_date', true);
                     $shortcode = get_post_meta(get_the_ID(), '_gtfa_gallery_shortcode', true);
                     $gallery_id = get_post_meta(get_the_ID(), '_gtfa_envira_gallery_id', true);
+
+                    // Format date with ordinal suffix
+                    if ($date) {
+                        $timestamp = strtotime($date);
+                        $day = date('j', $timestamp);
+                        $suffix = 'th';
+                        if ($day % 10 == 1 && $day != 11) $suffix = 'st';
+                        if ($day % 10 == 2 && $day != 12) $suffix = 'nd';
+                        if ($day % 10 == 3 && $day != 13) $suffix = 'rd';
+                        $formatted_date = date('F ', $timestamp) . $day . $suffix . date(' Y', $timestamp);
+                    }
                     ?>
                     <div class="gtfa-gallery-item">
                         <h3 class="gtfa-gallery-title"><?php the_title(); ?></h3>
                         <?php if ($date) : ?>
                             <div class="gtfa-gallery-date">
-                                <?php echo esc_html(date_i18n(get_option('date_format'), strtotime($date))); ?>
+                                <?php echo esc_html($formatted_date); ?>
                             </div>
                         <?php endif; ?>
                         <?php if ($gallery_id) :?>
